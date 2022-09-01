5a1a9f38426e3a5f26f6b6d1c8960c0a4a8b46de
arsalan manzoor <arsalan1998m@gmail.com>
2022-09-01 12:42:03 +0500

    post api with signup

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:rest_api/models/products_model.dart';
+// import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,92 +12,82 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<Products> getProducts() async {
-    const url = "https://webhook.site/92960c13-bc4d-4224-b286-1cd26d7b7ea3";
-    final response = await http.get(Uri.parse(url));
-    final data = jsonDecode(response.body);
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-    if (response.statusCode == 200) {
-      return Products.fromJson(data);
+  void register(String email, String password) async {
+    try {
+      Response response =
+          await post(Uri.parse('https://reqres.in/api/register'), body: {
+        'email': email,
+        'password': password,
+      });
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body.toString());
+        print(data);
+        print('account created successfully');
+      } else {
+        print('failed');
+      }
+    } catch (e) {
+      print(e.toString());
     }
-
-    return Products.fromJson(data);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: FutureBuilder<Products>(
-                future: getProducts(),
-                builder: ((context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    return ListView.builder(
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ListTile(
-                                title: Text(
-                                  snapshot.data!.data[index].shop!.name
-                                      .toString(),
-                                ),
-                                subtitle: Text(
-                                  snapshot.data!.data[index].shop!.shopemail
-                                      .toString(),
-                                ),
-                                leading: CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                  snapshot.data!.data[index].shop!.image
-                                      .toString(),
-                                )),
-                              ),
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height * .3,
-                                width: MediaQuery.of(context).size.width * 1,
-                                child: ListView.builder(
-                                    scrollDirection: Axis.horizontal,
-                                    itemCount: snapshot
-                                        .data?.data[index].images?.length,
-                                    itemBuilder: ((context, position) {
-                                      return Container(
-                                        height:
-                                            MediaQuery.of(context).size.height *
-                                                .3,
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                .5,
-                                        decoration: BoxDecoration(
-                                            image: DecorationImage(
-                                                image: NetworkImage(snapshot
-                                                    .data
-                                                    ?.data[index]
-                                                    .images?[position]
-                                                    .url as String))),
-                                      );
-                                    })),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      itemCount: snapshot.data?.data.length,
-                    );
-                  }
-                }),
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(hintText: 'Email'),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(hintText: 'Password'),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              GestureDetector(
+                onTap: () {
+                  register(emailController.text.toString(),
+                      passwordController.text.toString());
+                },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
