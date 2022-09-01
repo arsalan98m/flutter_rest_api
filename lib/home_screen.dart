c39060ee503a0c6e2ff216a447b75f1069bc4a22
arsalan manzoor <arsalan1998m@gmail.com>
2022-09-01 14:00:05 +0500

    image upload with rest api

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,11 @@
-import 'dart:convert';
+// import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 // import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,81 +15,124 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  File? image;
+  final _picker = ImagePicker();
+  bool showSpinner = false;
 
-  void register(String email, String password) async {
-    try {
-      Response response =
-          await post(Uri.parse('https://reqres.in/api/register'), body: {
-        'email': email,
-        'password': password,
+  Future getImage() async {
+    final pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print('no image selected');
+    }
+  }
+
+  Future<void> uploadImage() async {
+    setState(() {
+      showSpinner = true;
+    });
+
+    var stream = http.ByteStream(image!.openRead());
+    stream.cast();
+
+    var length = await image!.length();
+
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+
+    var request = http.MultipartRequest('post', uri);
+
+    request.fields['title'] = 'testing';
+
+    var multipart = http.MultipartFile(
+      'image',
+      stream,
+      length,
+    );
+
+    request.files.add(multipart);
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('image uploaded');
+      setState(() {
+        showSpinner = false;
       });
-
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body.toString());
-        print(data);
-        print('account created successfully');
-      } else {
-        print('failed');
-      }
-    } catch (e) {
-      print(e.toString());
+    } else {
+      print('failed');
+      setState(() {
+        showSpinner = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign up'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(28.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(hintText: 'Email'),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(hintText: 'Password'),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              GestureDetector(
-                onTap: () {
-                  register(emailController.text.toString(),
-                      passwordController.text.toString());
-                },
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(9),
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Upload Image'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    getImage();
+                  },
+                  child: Container(
+                    child: image == null
+                        ? const Center(
+                            child: Text('Pick Image'),
+                          )
+                        : SizedBox(
+                            child: Center(
+                              child: Image.file(
+                                File(image!.path).absolute,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
                   ),
-                  child: const Center(
-                    child: Text(
-                      'Sign up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    uploadImage();
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Upload',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
